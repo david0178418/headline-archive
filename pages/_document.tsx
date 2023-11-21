caa287f643f8bef56068ba4383fc09b674661b58
David Granado <davidjgranado@gmail.com>
2023-11-21 15:15:10 -0600

    YOLO!

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,25 +19,41 @@ class MyDocument extends Document {
 					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
 					<meta name="msapplication-TileColor" content="#da532c"/>
 					<meta name="theme-color" content="#ffffff"/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
 					{!IS_DEV && (
 						<>
 							<script async src="https://www.googletagmanager.com/gtag/js?id=UA-7940406-3"></script>
 							<script
+								type="module"
 								dangerouslySetInnerHTML={{
 									__html: `
-										window.dataLayer = window.dataLayer || [];
-										function gtag(){dataLayer.push(arguments);}
-										gtag('js', new Date());
-										gtag('config', 'UA-7940406-3');
+										// yolo!
+										import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+										import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
+										
+										const firebaseConfig = {
+											apiKey: "AIzaSyAhXzsCJBNNf1HLHLGu_vp65RKWT9sYyOI",
+											authDomain: "headline-archive.firebaseapp.com",
+											databaseURL: "https://headline-archive.firebaseio.com",
+											projectId: "headline-archive",
+											storageBucket: "headline-archive.appspot.com",
+											messagingSenderId: "471877828683",
+											appId: "1:471877828683:web:29eaea87354074ab830dce",
+											measurementId: "G-7JB06PKFHF"
+										};
+
+										// Initialize Firebase
+										const app = initializeApp(firebaseConfig);
+										const analytics = getAnalytics(app);
+										logEvent(analytics, 'page_load');
 									`
 								}}
 							/>
 						</>
 					)}
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
 				</body>
 			</Html>
 		);
